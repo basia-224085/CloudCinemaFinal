52387085cf2de2f5f1de540b043262329546d069
224085 Barbara Borkowska <224085@edu.p.lodz.pl>
2020-12-30 20:16:59 +0100

    Dodany service blob

--- CCS/Controllers/HomeController.cs
+++ CCS/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Azure.Storage.Blobs;
 
 namespace CCS.Controllers
 {
@@ -15,15 +16,16 @@ namespace CCS.Controllers
         private Repository repo = new Repository();
         public ActionResult Index()
         {
+            // Create a BlobServiceClient object which will be used to create a container client
+            //BlobServiceClient blobServiceClient = new BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=moviesposters;AccountKey=/W6OWyYwXeMGqpaweWBjTBKTyCrkqu+Pz4IkQJscLAz9iLxldCDZgV+OXh4YjjyL1Xn1fwHHvwl6tgER4euF3w==;BlobEndpoint=https://moviesposters.blob.core.windows.net/;TableEndpoint=https://moviesposters.table.core.windows.net/;QueueEndpoint=https://moviesposters.queue.core.windows.net/;FileEndpoint=https://moviesposters.file.core.windows.net/");
+
             List<Movies> movies = repo.getMoviesOfTheDay(1);
             var movieListViewModel = new MovieListViewModel(movies);
             return View(movieListViewModel);
         }
         public ActionResult Trailer()
         {
-            List<Movies> movies = repo.getMoviesOfTheDay(1);
-            var movieListViewModel = new MovieListViewModel(movies);
-            return View(movieListViewModel);
+            return View();
         }
         public ActionResult Logout()
         {
@@ -31,12 +33,13 @@ namespace CCS.Controllers
         }
         public ActionResult Tickets()
         {
-            int tickets = 0;
-            ViewBag.tickets = tickets;
+            
             return View();
         }
         public ActionResult Seats()
         {
+            var str = "test";
+            ViewBag.test = str;
             return View();
         }
         public ActionResult Reservation()
